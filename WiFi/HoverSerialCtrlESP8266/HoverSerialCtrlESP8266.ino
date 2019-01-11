812c51f3772973571171eb2a4b0f11e3a2dbdfb0
Gamer_Z <github@gz0.nl>
2019-01-11 23:09:21 +0100

    add code for ESP8266

---
+++ WiFi/HoverSerialCtrlESP8266/HoverSerialCtrlESP8266.ino
@@ -0,0 +1,170 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+
+#define MAX_SRV_CLIENTS 1
+const char* ssid = "GZX";
+const char* password = "******************";
+
+SoftwareSerial ss(5,4);
+
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+struct SerialCommand
+{
+    int16_t steer;
+    int16_t speed;
+} __attribute__ ((packed));
+
+SerialCommand command;
+
+void setup() 
+{
+    ss.begin(19200);
+    Serial.begin(19200);
+    
+    WiFi.begin(ssid, password);
+    
+    command.steer = 0;
+    command.speed = 0;
+}
+
+int current = 0;
+char data[5];
+
+int wifi_current = 0;
+char wifi_data[5];
+
+bool old_wifi_status = false;
+
+unsigned long time_x = 0;
+
+void loop() 
+{  
+    String data = "";
+    unsigned long time_y = millis();
+    if(time_y - time_x > 50)
+    {
+        data = "Speed: " + String(command.speed) + ", steer: " + String(command.steer);
+        time_x = time_y;
+        Serial.println(data);
+        ss.write((char*)&command, sizeof(command));
+    }
+
+//////////////////////////////////////////////////
+
+    while(Serial.available())
+    {
+        char c = Serial.read();
+        if(c == ';')
+        {
+            data[0] = ';';
+            current = 1;
+        }
+        else if(c != ';')
+        {
+            data[current++] = c;
+            if(current == 5)
+            {
+                memcpy((char*)&command, data+1, 4);
+            }
+        }
+    }
+    
+//////////////////////////////////////////////////
+
+    bool connected_wifi = WiFi.status() == WL_CONNECTED;
+
+    if(old_wifi_status != connected_wifi)
+    {
+        // smth changed
+        old_wifi_status = connected_wifi;
+
+        if(connected_wifi)
+        {
+            server.begin();
+        }
+        else
+        {
+            for(i = 0; i < MAX_SRV_CLIENTS; i++)
+            {
+                if (serverClients[i] && serverClients[i].connected())
+                {
+                    serverClient.stop();
+                }
+            }
+            server.stop();
+            WiFi.stop();
+            WiFi.begin(ssid, password);
+        }
+    }
+
+    if(connected_wifi)
+    {
+        uint8_t i;
+        // check if there are any new clients
+        if (server.hasClient())
+        {
+            bool added = false;
+            for(i = 0; i < MAX_SRV_CLIENTS; i++)
+            {
+                //find free/disconnected spot
+                if (!serverClients[i] || !serverClients[i].connected())
+                {
+                    if(serverClients[i])
+                    {
+                        serverClients[i].stop();
+                    }
+                    serverClients[i] = server.available();
+                    added = true;
+                    break;
+                }
+            }
+
+            if(!added)
+            {
+                // no free/disconnected spot so reject
+                WiFiClient serverClient = server.available();
+                serverClient.stop();
+            }
+        }
+        
+        //check clients for data
+        for(i = 0; i < MAX_SRV_CLIENTS; i++)
+        {
+            if (serverClients[i] && serverClients[i].connected())
+            {
+                while(serverClients[i].available()) 
+                {
+                    char c = serverClients[i].read();
+                    if(c == ';')
+                    {
+                        wifi_data[0] = ';';
+                        wifi_current = 1;
+                    }
+                    else if(c != ';')
+                    {
+                        wifi_data[current++] = c;
+                        if(wifi_current == 5)
+                        {
+                            memcpy((char*)&command, data+1, 4);
+                        }
+                    }                    
+                }
+            }
+        }
+        
+        //check UART for data
+        if(data.length())
+        {
+            for(i = 0; i < MAX_SRV_CLIENTS; i++)
+            {
+                if (serverClients[i] && serverClients[i].connected())
+                {
+                    serverClients[i].write(data.c_str(), data.length());
+                    delay(1);
+                }
+            }
+        }      
+    }
+}
---
+++ WiFi/JoystickController/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using XInput.Wrapper;
+using System.IO.Ports;
+using System.Threading;
+
+namespace JoystickController
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            SerialPort port = new SerialPort("COM5", 19200);
+
+            X.UpdatesPerSecond = 60;
+            Console.CancelKeyPress += delegate {
+                running = false;
+            };
+
+            X.Gamepad[] pads = {
+                X.Gamepad_1,
+                X.Gamepad_2,
+                X.Gamepad_3,
+                X.Gamepad_4
+            };
+
+            const int deadzone = 6000;
+            const int max = 32768;
+            const int max_corrected = max - deadzone;
+            const int divider = max_corrected / 100 + 1;
+
+            int old_speed = 0;
+            int old_steer = 0;
+
+            while (running)
+            {
+                Thread.Sleep(33);
+
+                if(!X.IsAvailable)
+                {
+                    continue;
+                }
+
+                int i = 0;
+                foreach (X.Gamepad pad in pads)
+                {
+                    if (pad.Update() && pad.IsConnected)
+                    {
+                        //Console.SetCursorPosition(0, i++);
+                        //Console.Write("(" + pad.LStick.X.ToString("D06") + ", " + pad.LStick.Y.ToString("D06") + ")");
+                        int steer = pad.RStick.X;
+                        int speed = pad.LStick.Y;
+
+                        if(steer < deadzone && steer > -deadzone)
+                        {
+                            steer = 0;
+                        }
+                        else
+                        {
+                            steer -= Math.Sign(steer) * deadzone;
+                            steer /= divider;
+                        }
+
+                        if(speed < deadzone && speed > -deadzone)
+                        {
+                            speed = 0;
+                        }
+                        else
+                        {
+                            speed -= Math.Sign(speed) * deadzone;
+                            speed /= divider;
+                        }
+
+                        //Console.Write("(" + steer.ToString("D04") + ", " + speed.ToString("D04") + ")");
+
+                        if(!port.IsOpen)
+                        {
+                            port.Open();
+                        }
+
+                        if(port.IsOpen)
+                        {
+                            if ((old_speed != speed || old_steer != steer))
+                            {
+                                old_speed = speed;
+                                old_steer = steer;
+
+                                short steer16 = (short)steer;
+                                steer16 *= 10;
+                                short speed16 = (short)speed;
+                                speed16 *= 10;
+
+                                byte[] steerBytes = BitConverter.GetBytes(steer16);
+                                byte[] speedBytes = BitConverter.GetBytes(speed16);
+
+                                /*if (BitConverter.IsLittleEndian)
+                                {
+                                    Array.Reverse(steerBytes);
+                                    Array.Reverse(speedBytes);
+                                }*/
+
+                                byte[] total = new byte[5]
+                                {
+                                    (byte)';',
+                                    steerBytes[0],
+                                    steerBytes[1],
+                                    speedBytes[0],
+                                    speedBytes[1]
+                                };
+
+                                port.Write(total, 0, 5);
+
+                                //Console.WriteLine(BitConverter.ToString(total));
+                            }
+
+                            if(port.BytesToRead > 0)
+                            {
+                                byte[] bytes = new byte[port.BytesToRead];
+                                port.Read(bytes, 0, bytes.Length);
+                                Console.Write(System.Text.Encoding.ASCII.GetString(bytes));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
